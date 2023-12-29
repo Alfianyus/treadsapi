520f51df87945bd19e5bb3018971f9ce8fff8ae8
Alfianyus <alfian.yuska1@gmail.com>
2023-12-29 15:06:35 +0700

    firstcommit

---
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthenticationController extends Controller
+{
+    public function register(RegisterRequest $registerRequest)
+    {
+        try {
+           $registerRequest->validated();
+
+           $user = User::create([
+            'name' => $registerRequest->name,
+            'username'=> $registerRequest->username,
+            'email' => $registerRequest->email,
+            'password' => Hash::make($registerRequest->password)
+
+           ]);
+           $token = $user->createToken('threads')->plainTextToken;
+
+           return response([
+                'user' => $user,
+                'token' => $token
+           ], 201);
+        } catch (\Exception $e) {
+            return response([
+                'message' => $e->getMessage(),
+            ], 500);
+            //throw $th;
+        }
+    }
+
+    public function login(LoginRequest $loginRequest)
+    {
+        try {
+           $loginRequest->validated();
+
+           //check user
+           $user = User::whereUsername($loginRequest->username)->first();
+           if(!$user || !Hash::check($loginRequest->password, $user->password)){
+            return response([
+                'message' => 'Invalid Credentials'
+            ], 422);
+           }
+
+           $token = $user->createToken('threads')->plainTextToken;
+
+           return response([
+                'user' => $user,
+                'token' => $token
+           ], 200);
+
+
+        } catch (\Exception  $e) {
+            return response([
+                'message' => $e->getMessage()
+            ], 500);
+            //throw $th;
+        }
+
+    }
+}
